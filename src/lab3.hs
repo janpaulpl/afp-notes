cffbd1bfb133822179e74c46645d79b7895e2d7c
jpVinnie <janpaul.ramos@protonmail.com>
2023-07-04 11:42:16 -0400

    work

--- src/lab3.hs
+++ src/lab3.hs
@@ -1,4 +1,29 @@
 -- [Lab](https://uu-afp.github.io/as3.html)
 
+{-# LANGUAGE GADTs, TypeFamilies #-}
+
 -- 0. Contracts
--- Exercise 0.1: 
+
+-- data Contract :: * -> * where
+--   Pred :: (a -> Bool) -> Contract a
+--   Fun  :: Contract a -> Contract b -> Contract (a -> b)
+
+
+-- assert :: Contract a -> a -> a
+-- assert (Pred p)       x = if p x then x else error "contract violation"
+-- assert (Fun pre post) f = assert post . f . assert pre
+
+-- Exercise 0.1: Define a contract `true :: Contract a` such that for all values `x` the equation `assert true x == x` holds. Prove this using equational reasoning.
+
+true :: Contract a
+true = Pred (\a -> True)
+
+-- Exercise 0.2: Adapt the function `assert` to the new type of `DFun`.
+
+data Contract :: * -> * where
+  Pred :: (a -> Bool) -> Contract a
+  DFun :: Contract a -> (a -> Contract b) -> Contract (a -> b)
+
+assert :: Contract a -> a -> a
+assert (Pred p)       x = if p x then x else error "contract violation"
+assert (DFun pre post) f =  assert post 1 . f . assert pre
